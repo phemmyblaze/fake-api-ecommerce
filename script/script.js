978f25c99cca12b39388829d28ad6c5eded2e0c4
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-03-07 15:26:54 +0100

    Total price function added

--- script/script.js
+++ script/script.js
@@ -1,68 +1,87 @@
+// let product = 'products';
+// product =  'items';
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let products = document.querySelector(".products");
       async function getProducts(url) {
         let data = await fetch(url);
         let result = await data.json();
-        
-        for (let i = 0; i < result.length; i++) {
-
-            let images = result[i].images[1];
-            let title =  result[i].title;
-            let category = result[i].category.name;
-            let description = result[i].description;
-
-           products.innerHTML += `
+        console.log(result);
+        result.forEach(item => {
+            let photo = item.images != 'doggy.jpg' ? item.images : "./error.jpg";
+            products.innerHTML += `
                 <div class="product">
-                    <img src="${images}" alt="" class="product-img">
-                    <h2 class="product-title">${title}</h2>
-                    <h4 class="product-category">${category}</h4>
-                    <p class="product-description">${description}</p>
+                    <img src="${photo}" alt="" class="product-img">
+                    <h2 class="product-title">${item.title}</h2>
+                    <h4 class="product-category">${item.category.name}</h4>
+                    <p class="product-description">${item.description}</p>
                     <div class="product-price-container">
-                        <h3 class="product-price">$ ${result[i].price}</h3>
+                        <h3 class="product-price">$ ${item.price}</h3>
                         
-                        <a href="#" data-productId="${result[i].id}" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
+                        <button onClick="addCart(${item.id})" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></button>
                         
                     </div>
                 </div>
 
             `;
-            
-        }
-
-    }
-
-    // let btn = document.createElement('btn')
-
-    // btn.textContent = "Add to cart"
-    // btn.className= "add"
-
-    // btn.addEventListener("click", function (e) {
-    //     console.log('btn clicked')
-    // })
-
-    // document.querySelector(".products").querySelector(".product-price-container").appendChild(btn)
-
-    ///ADD TO CART FUNCTION
-    let btn = document.querySelectorAll("add-to-cart");
-    console.log(btn);
-    
-    btn.forEach(btn => {
-        btn.addEventListener('click', () => {
-          alert('add to cart');
         });
-      })
-
-    // const list = document.getElementsByClassName("add-to-cart")[0];
-    // console.log(list);
-    // list.getElementsByClassName("")[0].innerHTML = "Milk";
-
-  
-
-//   getProducts("https://fakestoreapi.com/products");
+    }
   getProducts("https://api.escuelajs.co/api/v1/products");
-//   getProducts('https://api.storerestapi.com/products');
+
 });
 
+// function addCart(item) {
+//     console.log(item);
+// }
+
+////FECTCHING EACH ITEM AND SAVING TO LOCAL STORAGE
+let cartItem = [];
+async function addCart(item){
+    // console.log(item);
+    let data = await fetch(`https://api.escuelajs.co/api/v1/products/${item}`);
+    let result = await data.json()
+    
+    let check_cart = JSON.parse(localStorage.getItem("carts"));
+
+    if (check_cart == null){
+        let cart =  {
+            image:result.images,
+            title:result.title,
+            price:result.price
+
+        };
+        cartItem.push(cart);
+        console.log(cartItem);
+        localStorage.setItem("carts", JSON.stringify(cartItem));
+    } else {
+        let newcart = {
+            image:result.images,
+            title:result.title,
+            price:result.price,
+        } 
+        cartItem.push(newcart);
+        localStorage.setItem("carts", JSON.stringify(cartItem));
+    }
+    
+
+} 
+////PERSISTENCE
+window.onload = function () {
+    let cart_Item = JSON.parse(localStorage.getItem("carts"));
+
+    if (cart_Item!= null){
+        localStorage.setItem("newCarts", JSON.stringify(cart_Item));
+
+        let items = JSON.parse(localStorage.getItem("newCarts"));
+
+        items.forEach(item => {
+            cartItem.push(item);
+        })
+        localStorage.removeItem("newCarts");
+    }
+}
 
 
 //.length > 20 ? description.substring(0, 90).concat('...more') : description
